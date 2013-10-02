20a0731b7987f0680efa94235957a74dc43feb00
david942j <david942j@gmail.com>
2013-10-02 23:38:59 +0800

    server can support register/login

--- TaipeiHot/JazzChat/Server/Account.java
+++ TaipeiHot/JazzChat/Server/Account.java
@@ -14,30 +14,27 @@ public class Account extends Thread{
 	//User Data
 	static public int TotalID = 0;
 	public int id;
-	protected String email, nickname,status;
+	public String email, nickname,status;
 	protected String password;
 	protected ArrayList<User> Friends = new ArrayList<User>();
 	
 	//Communicate
-	private Socket socket;
+	public Socket socket;
 	private Deque<Byte> BufferInput = new LinkedList<Byte>();
 	private Queue<String> Messages = new LinkedList<String>();
 	BufferedInputStream in;
-	Thread GetMessageToBuffer;
+	Thread GetMessageToBuffer, startThread;
 	
 	
 	public Account(){}
-	public Account(String E,String P){
-		id=++TotalID;
-		email = E;
-		password = P;
-		nickname = E;
-	}
-	public Account(Socket _s) throws IOException{
-		id=++TotalID;
+	public Account(Socket _s){
 		socket=_s;
 		//socket.setSoTimeout(15000);
-		in = new BufferedInputStream(socket.getInputStream());
+		try {
+			in = new BufferedInputStream(socket.getInputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		openInputThread();
 	}
 	public void SetNickname(String N){
@@ -61,12 +58,12 @@ public class Account extends Thread{
 		for(int i=0;i<length;i++)
 			b[i]=BufferInput.pollFirst();
         data += new String(b, 0, length);
+        System.out.println(data);
         Messages.add(data);
-		System.out.println(length+" "+data);
         return true;
 	}
 	private void openInputThread(){
-		System.out.print("new Thread");
+		System.out.println("new Thread");
 		GetMessageToBuffer=new Thread(new Runnable(){
 			@Override
 			public void run() {
@@ -87,26 +84,89 @@ public class Account extends Thread{
 				}
 			}
 		});
+		startThread = new Thread(new Runnable(){
+			@Override
+			public void run(){
+				while(!trylogin());
+				while(true){
+		        	String msg = getMessage();
+		        	if(msg != "")
+		        		System.out.print(msg);
+		        	try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 		GetMessageToBuffer.start();
+		startThread.start();
 	}
 	
 	public String getMessage(){
-		if(Messages.isEmpty())return "";
+		while(Messages.isEmpty()){
+			while(!ParseByte()){
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 		return Messages.poll();
 	}
 	
 	public void run(){
-		while(true){
-			while(ParseByte());
-        	String first_msg = getMessage();
-        	if(first_msg != "")
-        		System.out.print(first_msg);
-        	try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		
+	}
+	
+	private void clone(Account tmp){ // copy all data except socket
+		this.nickname = tmp.nickname;
+		this.Friends  = tmp.Friends;
+		this.status   = tmp.status;
+		this.id       = tmp.id;
+	}
+	private Boolean trylogin(){// TODO commandMgr
+		String cmd=getMessage();
+		if(cmd.equals("register")){
+			email = getMessage();
+			password = getMessage();
+			id = ++Account.TotalID;
+			nickname = email;
+			status = "How are you today?";
+			Server.clientMap.put(email, id);
+	        Server.userArray.add(this);
+	        if(Account.TotalID+1 != Server.userArray.size())
+	        	System.out.println("Total id and array size not match");
+	        System.out.println("Register success! Become User "+id);
+	        return true;
+		}
+		else if(cmd.equals("login")){
+			email = getMessage();
+			password = getMessage();
+			if(Server.clientMap.containsKey(email)){
+				System.out.println("get index in map = "+Server.clientMap.get(email));
+				Account tmp = Server.userArray.get(Server.clientMap.get(email));
+				if(password.equals(tmp.password)){
+					clone(tmp);
+					System.out.println("login success, become User "+this.id);
+					//TODO Send success message
+					return true;
+				}
+				else{
+					System.out.println("login fail, wrong password");
+					//TODO send fail message
+					return false;
+				}
+		    }
+			else{
+				System.out.println("login fail, wrong email");
+				//TODO send fail message
+				return false;
 			}
 		}
+		else System.out.println("In login, what are you talking QQ");
+		return false;
 	}
 }
