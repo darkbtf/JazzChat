01b65844c2e3f1e6489269994b23fd02b3a46f80
david942j <david942j@gmail.com>
2013-10-04 15:23:21 +0800

    record user data to file

--- TaipeiHot/JazzChat/Server/Server.java
+++ TaipeiHot/JazzChat/Server/Server.java
@@ -1,13 +1,23 @@
 package TaipeiHot.JazzChat.Server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 import TaipeiHot.JazzChat.Parameter;
+import TaipeiHot.JazzChat.Util;
 
 public class Server {
     static public Map<String, Integer> clientMap;
@@ -23,12 +33,49 @@ public class Server {
     	Server server = new Server();
     	(new SocketThread()).start();
     	server.equals(new Integer(123)); // to remove unused warning = =
+    	Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+            	writeUserData();
+            }
+        }, "Shutdown-thread"));
     }
     
-    private void readUserData(){// TODO read from SQL
+    static private void readUserData(){// TODO read from SQL
+    	try {
+    		DataInputStream in = new DataInputStream(new FileInputStream("userData.dat"));
+    		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+    		int n= Integer.valueOf(br.readLine());
+    		for(int i=0;i < n; i++) {
+    			String s = br.readLine();
+    			Account a = new Account();
+    			a.email = s;
+    			a.password = s;
+    			accountArray.add(a);
+    		}
+			br.close();
+		} catch (FileNotFoundException e) {
+			Util.errorReport("In read user data, userData not found");
+		}
+    	catch(Exception e){Util.errorReport("In read user data, Exception");}
+    }
+    
+    static public void writeUserData(){
+    	try {
+    		System.out.print("writing user data\n");
+    		DataOutputStream in = new DataOutputStream(new FileOutputStream("userData.dat"));
+    		BufferedWriter br = new BufferedWriter(new OutputStreamWriter(in));
+    		br.write((accountArray.size()-1)+"\n");
+    		for(int i=1;i<accountArray.size();i++){
+    			Account a = accountArray.get(i);
+    			br.write(a.email+"\n"+a.password+"\n");
+    		}
+			br.close();
+		} catch (FileNotFoundException e) {
+			Util.errorReport("In write user data, userData not found");
+		}
+    	catch(Exception e){Util.errorReport("In write user data, Exception");}
     	
     }
-    
     private void initClientMap(){
     	clientMap= new HashMap<String, Integer>();
     	for(Account a:accountArray)
