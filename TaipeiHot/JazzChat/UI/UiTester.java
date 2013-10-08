8b19f838bb65b4d0a5044987820ebae0ee9d78a7
b92paul <b92paul@gmail.com>
2013-10-08 09:47:22 +0800

    newRoom

--- TaipeiHot/JazzChat/UI/UiTester.java
+++ TaipeiHot/JazzChat/UI/UiTester.java
@@ -15,20 +15,22 @@ import java.io.InputStreamReader;
  */
 public class UiTester {
     public static void main(String args[]) throws IOException{
-        RoomWindow a=new RoomWindow();
+        RoomWindow a=new RoomWindow(2);
         a.show(true);
-        a.getMessege("hahaha");
+        a.getMessege("tt","hahaha");
         MainWindow xx=new MainWindow();
-        
-        xx.setVisible(true);
+        xx.newRoom(123);
+        xx.newRoom(234);
+
         xx.loginShow();
         String aaa="lalal";
         //xx.closeDialog();
         while(true)
         {
+            
             BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
             String line=buffer.readLine();
-            a.getMessege(line);
+            a.getMessege("tt",line);
         }
         
     }
