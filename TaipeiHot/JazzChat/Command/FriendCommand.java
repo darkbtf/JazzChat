e201ba9a3bacb1aab7c916aa8dc0e5a76253426e
darkbtf <darkbtf@gmail.com>
2013-10-10 13:11:04 +0800

    jizzer

--- TaipeiHot/JazzChat/Command/FriendCommand.java
+++ TaipeiHot/JazzChat/Command/FriendCommand.java
@@ -14,7 +14,7 @@ public class FriendCommand implements Command {
 			String greeting = Client.getMessage();
 			User tmpUser = new User(userName, greeting);
 			Client.friendsToAdd.add(tmpUser);
-			Client.mainWindow.pendingListShow(tmpUser);
+			// Client.mainWindow.pendingListShow(tmpUser);
 			Client.mainWindow.acceptFriendShow(userId, userName, greeting);
 		} else if (cmd.equals("response")) {
 			String userName = Client.getMessage();
@@ -31,9 +31,11 @@ public class FriendCommand implements Command {
 			int userId = Integer.parseInt(Client.getMessage());
 			String userName = Client.getMessage();
 			String status = Client.getMessage();
+			boolean online = Client.getMessage().equals("true") ? true : false;
 			User tmpUser = new User(userName, status);
 			tmpUser.id = userId;
-			// System.out.println(Integer.toString(userId) + " " + userName);
+			tmpUser.setOnline(online);
+			System.out.println(Integer.toString(userId) + " " + userName);
 			Client.userSet.put(userId, tmpUser);
 			Client.mainWindow.friendShow(tmpUser);
 		} else if (cmd.equals("online")) {
