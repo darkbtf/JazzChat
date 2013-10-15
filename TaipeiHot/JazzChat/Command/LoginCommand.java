7625eb921b2e69a95bf69d0914418124b93cce19
b92paul <b92paul@gmail.com>
2013-10-15 19:19:07 +0800

    things

--- TaipeiHot/JazzChat/Command/LoginCommand.java
+++ TaipeiHot/JazzChat/Command/LoginCommand.java
@@ -2,6 +2,10 @@ package TaipeiHot.JazzChat.Command;
 
 import TaipeiHot.JazzChat.User;
 import TaipeiHot.JazzChat.Client.Client;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class LoginCommand implements Command {
 	@Override
@@ -27,7 +31,13 @@ public class LoginCommand implements Command {
 			tmpUser.setProfilePicUrl(picUrl);
 			Client.userSet.put(id, tmpUser);
 			Client.user = tmpUser;
-			Client.mainWindow.loginSuccess();
+                    try {
+                        Client.mainWindow.loginSuccess();
+                    } catch (MalformedURLException ex) {
+                        Logger.getLogger(LoginCommand.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(LoginCommand.class.getName()).log(Level.SEVERE, null, ex);
+                    }
 		}
 	}
 }
