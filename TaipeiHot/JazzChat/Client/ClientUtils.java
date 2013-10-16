1461c1ac5241a3e68bdbaa77f7b89f21e13be628
darkbtf <darkbtf@gmail.com>
2013-10-16 23:55:20 +0800

    meow

--- TaipeiHot/JazzChat/Client/ClientUtils.java
+++ TaipeiHot/JazzChat/Client/ClientUtils.java
@@ -13,9 +13,10 @@ public class ClientUtils {
 	public static void sendStringsToServer(OutputStream out, String[] strs) {
 		synchronized (out) {
 			for (String str : strs) {
+				if (str.length() > 250) {
+					str = str.substring(0, 250);
+				}
 				byte[] strBytes = str.getBytes();
-				if (strBytes.length > 500)
-					continue;
 				byte[] length = Util.intToByteArray(strBytes.length);
 				try {
 					out.write(length);
